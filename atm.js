61ac2d843ff57818aa139801750ad1d64b399bd8
jcyoder <kajcyoder@comcast.net>
2014-05-31 14:41:16 -0600

    implemented functions

--- atm.js
+++ atm.js
@@ -1,27 +1,48 @@
-$(document).ready(function() {
-    
-   
-    function computeATM(withdraw, balance) {
-        var validwithdraw = checkWithDraw(withdraw);
-        if (validwithdraw) {
-            checkBalance(balance);
-        } else {
-            //invalid input
-        }
-        //is withdraw < (balance - .50)
-        //return true or false 
-    }
-    
-    function checkWithDraw(withdraw) {
-        //check that withdraw is a multiple of 5
-        //check that withdraw is > 0 and < 2000
-        //return true or false
-    }
-    
-    function checkBalance(balance) {
-        //check to make sure not a negative number
-    }
-    
-    
 
-});
+function getWithdrawl() {
+	console.log("Called getWithdrawl");
+	var withdraw = document.getElementById('atm').value;
+	var balance = document.getElementById('balance').value;
+	console.log(withdraw);
+	console.log(balance);
+	var bal = computeATM(withdraw, balance);
+	alert("balance: " + bal);
+	
+}
+
+function checkWithDraw(withdraw) {
+	if (withdraw % 5) {
+		return false;
+	}
+	if (withdraw > 0 && withdraw < 2000) {
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+function checkBalance(balance) {
+	if (balance < 0) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function computeATM(withdraw, balance) {
+	var validwithdraw = checkWithDraw(withdraw);
+	if (validwithdraw) {
+		if (checkBalance(balance)) {
+			if (withdraw < (balance - 0.50)) {
+				var newbalance = balance - withdraw - 0.50;
+				console.log(newbalance);
+				return newbalance;
+			} else {
+				return balance;
+			}
+		}
+	} else {
+		return balance;
+	}
+}
